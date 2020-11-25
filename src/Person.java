0ca58caabdce2bd9575eadd440d44ca8ff5f509e
ustbly <ustbly2021@126.com>
2020-11-25 14:20:03 +0800

    进行第一次数据的提交

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+/**
+ * @Author LinYue
+ * @Email ustb2021@126.com
+ * @Create 2020-11-25 14:15
+ */
+public class Person {
+    private String name;
+    private int age;
+}
---
+++ src/Student.java
@@ -0,0 +1,7 @@
+/**
+ * @Author LinYue
+ * @Email ustb2021@126.com
+ * @Create 2020-11-25 14:17
+ */
+public class Student {
+}
